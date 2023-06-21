82fbce2179b252fce083492f937477b02bb4cae6
Ars Vladimirov <grokhael@gmail.com>
2023-06-21 12:16:41 +0400

    add readme + refactor

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # download-teamfiles
-Download URL's directly to instance
+Download URL's files directly to instance.
+The structure of the json is identical to `DOWNLOAD_ORIGINAL_URL`:
+
+``` json
+{
+    "Annotations.zip": "https://url.com/Annotations.zip",
+    "arhive1.zip": "https://url.com/archive1.zip",
+    "ArChiiive2.tar": "https://url.com/ArChiiive2.tar"
+}
+```
--- src/ui.py
+++ src/ui.py
@@ -1,93 +1,76 @@
-import src.globals as g
-
-import shutil
-import supervisely as sly
-from urllib.parse import urlparse, unquote
-import requests
-from tqdm import tqdm
 import json
 import os
+import shutil
+from urllib.parse import unquote, urlparse
 
+import requests
+from tqdm import tqdm
+
+import src.globals as g
+import supervisely as sly
 from supervisely.app.widgets import (
-    Input,
-    Container,
-    Card,
-    Text,
     Button,
-    Progress,
+    Card,
+    Checkbox,
+    Container,
     DatasetThumbnail,
-    ProjectThumbnail,
     Editor,
+    Input,
+    Progress,
+    ProjectThumbnail,
     SlyTqdm,
-    Checkbox
+    Text,
 )
 
 progress_bar = Progress(show_percents=False)
 
+input_tf_dest_dir = Input(placeholder="Please input here destination directory in Team files")
+editor = Editor(initial_text='{"archive.zip":"https://url.com/archive.zip"}')
+button_download = Button(text="Download")
 
-
-input_tf_dest_dir = Input(placeholder='Please input here result directory in team files')
-editor = Editor(initial_text='{"url_name":"https://url.com/archive.zip"}')
-button_download = Button(text='Download')
-
-
-
-# progress_bar_download = SlyTqdm()
 
 card_1 = Card(
     title="Download from url",
-    content=Container(
-        widgets=[
-            input_tf_dest_dir,
-            editor,
-            button_download,
-            progress_bar
-        ]
-    ),
+    content=Container(widgets=[input_tf_dest_dir, editor, button_download, progress_bar]),
 )
 
 progress_bar.hide()
-# thumbnail.hide()
 
 
 @button_download.click
 def download():
-
     data_dict = json.loads(editor.get_text())
 
-    # url = "http://example.com/file.txt"  # Replace with the URL of the file you want to download
-    tf_destination_directory = os.path.join(g.STORAGE_DIR, 'downloads')  # Replace with the desired path to save the file
-
     progress_bar.show()
-    with progress_bar(message=f"Iterating dict with URLs", total=len(data_dict.values())) as bigpbar:
-
-        for name, url in data_dict.items():
-
-            parsed_url = urlparse(url)
-            file_name = os.path.basename(parsed_url.path)
-            file_name = unquote(file_name) 
-            destination_path = os.path.join(tf_destination_directory, name, file_name)
-
-            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-
+    with progress_bar(
+        message=f"Iterating dict with URLs", total=len(data_dict.values())
+    ) as bigpbar:
+        for file_name, url in data_dict.items():
+            local_path = os.path.join(g.STORAGE_DIR, file_name)
             response = requests.get(url, stream=True)
 
-            total_size = int(response.headers.get('content-length', 0))
+            total_size = int(response.headers.get("content-length", 0))
             block_size = 1024
 
-            with tqdm(message=f"Downloading {name} to buffer", total=total_size, unit='B', unit_scale=True) as pbar:
-                with open(destination_path, 'wb') as file:
+            with tqdm(
+                message=f"Downloading {file_name} to buffer",
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+            ) as pbar:
+                with open(local_path, "wb") as file:
                     for data in response.iter_content(block_size):
                         file.write(data)
                         pbar.update(len(data))
-                        
+
             tf_dst_path = os.path.join(input_tf_dest_dir.get_value(), file_name)
-            with tqdm(message=f"Uploading {name} to Team files", total=total_size, unit='B', unit_scale=True) as pbar:
-                g.api.file.upload(g.TEAM_ID,destination_path, tf_dst_path, progress_cb=pbar)
+            with tqdm(
+                message=f"Uploading {file_name} to Team files",
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+            ) as pbar:
+                g.api.file.upload(g.TEAM_ID, local_path, tf_dst_path, progress_cb=pbar)
 
-            shutil.rmtree(path=os.path.dirname(destination_path))
-
+            os.remove(local_path)
             bigpbar.update(1)
-
-
-
